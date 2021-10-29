98530e0a4c8dc8ccf8a2d168a4776343b40d93b7
GitHub Action <action@github.com>
2021-10-29 08:23:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/picimako/devkitplus/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/picimako/devkitplus/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.picimako.devkitplus.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.picimako.devkitplus.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/picimako/devkitplus/services/MyProjectService.kt
+++ src/main/kotlin/com/github/picimako/devkitplus/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.picimako.devkitplus.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.picimako.devkitplus.MyBundle
 
 class MyProjectService(project: Project) {
 
