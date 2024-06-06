62d4eccd80bfdc57bac885e4dc14eed124843c1e
Tamas Balog <picimako@protonmail.com>
2024-06-06 08:58:38 +0200

    Delete unused fields from PlatformPsiCache

--- src/main/kotlin/com/picimako/justkitting/PlatformPsiCache.kt
+++ src/main/kotlin/com/picimako/justkitting/PlatformPsiCache.kt
@@ -8,9 +8,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiField
-import com.intellij.psi.PsiSubstitutor
 import com.intellij.psi.search.ProjectScope
-import com.intellij.psi.util.MethodSignature
 
 /**
  * Project service to store Psi elements referencing the [Service] annotation, and properties of [Service.Level].
@@ -23,11 +21,6 @@ class PlatformPsiCache(val project: Project) {
     val serviceAnnotation: PsiClass? by lazy { findClass(PlatformNames.SERVICE_ANNOTATION) }
     val serviceLevelProject: PsiField? by lazy { findClass(PlatformNames.SERVICE_LEVEL)!!.findFieldByName("PROJECT", false) }
     val serviceLevelApp: PsiField? by lazy { findClass(PlatformNames.SERVICE_LEVEL)!!.findFieldByName("APP", false) }
-    val createOptionsPanelMethod: MethodSignature by lazy { findClass(PlatformNames.INSPECTION_PROFILE_ENTRY)!!.findMethodsByName("createOptionsPanel", false)[0].getSignature(PsiSubstitutor.EMPTY) }
-    val multipleCheckboxOptionsPanel: PsiClass? by lazy { findClass(PlatformNames.MULTIPLE_CHECKBOX_OPTIONS_PANEL) }
-    val singleCheckboxOptionsPanel: PsiClass? by lazy { findClass(PlatformNames.SINGLE_CHECKBOX_OPTIONS_PANEL) }
-    val singleIntegerFieldOptionsPanel: PsiClass? by lazy { findClass(PlatformNames.SINGLE_INTEGER_FIELD_OPTIONS_PANEL) }
-    val conventionOptionsPanel: PsiClass? by lazy { findClass(PlatformNames.CONVENTION_OPTIONS_PANEL) }
     val callMatcher: PsiClass? by lazy { findClass(PlatformNames.CALL_MATCHER) }
 
     private fun findClass(name: String): PsiClass? = JavaPsiFacade.getInstance(project).findClass(name, ProjectScope.getLibrariesScope(project)) 
