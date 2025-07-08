d751c6efb4a42ed522cb2720478f87bd60977040
Tamás Balog <picimako@protonmail.com>
2025-07-08 21:15:41 +0200

    Update copyright year to 2025, and make spacing consistent

--- src/test/java/com/picimako/justkitting/intention/state/MakeKotlinPersistentStateComponentIntentionTest.kt
+++ src/test/java/com/picimako/justkitting/intention/state/MakeKotlinPersistentStateComponentIntentionTest.kt
@@ -1,4 +1,5 @@
-//Copyright 2024 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+//Copyright 2025 Tamás Balog. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 package com.picimako.justkitting.intention.state
 
 import com.picimako.justkitting.JustKittingTestBase
